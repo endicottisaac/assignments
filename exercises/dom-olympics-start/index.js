1592ba64060d64ff53ebc3ab4d7578f089bd27f5
Isaac Endicott <endicottkai@gmail.com>
2023-04-24 12:12:17 -0600

    finished qualifier section on exercise

---
+++ exercises/dom-olympics-start/index.js
@@ -0,0 +1,13 @@
+//Qualifier 
+
+var newHeader = document.getElementById("header");
+newHeader.classList.add("header");
+newHeader.innerHTML = "<h1>JavaScript Made This!!</h1>";
+newHeader.innerHTML += "<p><span>Isaac Endicott</span> wrote the JavaScript</p>";
+
+var nameSpan = document.querySelector("span");
+nameSpan.classList.add("name");
+
+
+var firstLeftMessage = document.querySelector("left");
+firstLeftMessage.style.backgroundColor = "blue";
