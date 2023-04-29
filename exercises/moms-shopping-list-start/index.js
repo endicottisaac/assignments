1d14e51670be198eeaea73a4955863e05a0a27ad
Isaac Endicott <endicottkai@gmail.com>
2023-04-28 20:29:21 -0600

    added a new function to create div and button

---
+++ exercises/moms-shopping-list-start/index.js
@@ -0,0 +1,36 @@
+const form = document.addItem; 
+const mainList = document.getElementById("list");
+
+
+form.addEventListener('submit', function(e){
+    event.preventDefault()
+    const newItem = createListItem(e)
+    mainList.append(newItem)
+    form.title.value = ""
+})
+
+const createListItem = e => {
+
+    const subItem = document.createElement('li');
+
+    const div = document.createElement('div')
+    div.textContent = form.title.value;
+    const editButton = document.createElement('button')
+    editButton.textContent = 'edit';
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X'
+    
+    subItem.appendChild(div)
+    subItem.appendChild(editButton)
+    subItem.appendChild(deleteButton)
+
+    
+    
+    return subItem
+}
+
+
+
+
+
+
