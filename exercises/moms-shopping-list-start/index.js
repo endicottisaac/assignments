dbd4ff37321e60514e0e5228ac93272c30ea7596
Isaac Endicott <endicottkai@gmail.com>
2023-04-29 12:35:27 -0600

    added delete button functionality to list

--- exercises/moms-shopping-list-start/index.js
+++ exercises/moms-shopping-list-start/index.js
@@ -15,19 +15,28 @@ const createListItem = e => {
 
     const div = document.createElement('div')
     div.textContent = form.title.value;
-    const editButton = document.createElement('button')
-    editButton.textContent = 'edit';
+    // const editButton = document.createElement('button')
+    // editButton.textContent = 'edit';
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'X'
     
     subItem.appendChild(div)
-    subItem.appendChild(editButton)
+   // subItem.appendChild(editButton)
     subItem.appendChild(deleteButton)
 
-    
+
     
     return subItem
 }
+mainList.addEventListener("click", function(e){
+    if(e.target.textContent === 'X'){
+        e.target.parentElement.remove()
+    }
+})
+
+
+
+
 
 
 
