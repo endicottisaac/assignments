43eda8cf15f179444ca9fb8021e4d829ad463d82
Isaac Endicott <endicottkai@gmail.com>
2023-05-07 00:25:21 -0600

    finished challeges

--- exercises/level-one/string-methods-exercise/main.js
+++ exercises/level-one/string-methods-exercise/main.js
@@ -17,8 +17,29 @@ const findMiddleIndex = (word) => {
     const halfIndex = wordLength / 2;
     const roundedIndex = Math.floor(halfIndex);
 
-    console.log(roundedIndex);
+    return roundedIndex;
 }
 
-findMiddleIndex('hello'); 
+console.log(findMiddleIndex('hello')); 
 
+//Write a function that uses slice() and the other functions you've written to return the first half of the given string.
+
+const returnFirstHalf = (word) => {
+    const number = findMiddleIndex(word);
+    const slicedWord = word.slice(0, number);
+    return slicedWord;
+
+}
+console.log(returnFirstHalf('how do you do'))
+
+//Write a function that takes a string as a parameter and returns that string where the first half is capitalized, and the second half is lowercase.
+const splitCase = (word) => {
+    const number = findMiddleIndex(word)
+    const firstHalf = returnFirstHalf(word).toUpperCase();
+    const getSecondHalf = word.slice(number)
+    const secondHalf = getSecondHalf.toLowerCase()
+    const completeWord = firstHalf + secondHalf;
+
+    return completeWord
+}
+console.log(splitCase('iiii'));
