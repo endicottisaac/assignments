b567b8d930098d2cabb145dff7d22db06544349a
Isaac Endicott <endicottkai@gmail.com>
2023-05-15 09:34:17 -0600

    finished second challenge

--- exercises/level-one/problem-solving-practice/main.js
+++ exercises/level-one/problem-solving-practice/main.js
@@ -1,3 +1,5 @@
+//Largest number challenge
+
 const largest = (array) => {
     let largestNum = 0;
     for(let i = 0; i < array.length; i++){
@@ -10,6 +12,21 @@ const largest = (array) => {
 }
 
 
-console.log(largest([6, 13, 250, 3]))
-console.log(largest([3, 5, 2, 8, 1]))
-console.log(largest([-13, 40, 3, 0, 19, 22])) 
+console.log(largest([6, 13, 250, 3]));
+console.log(largest([3, 5, 2, 8, 1]));
+console.log(largest([-13, 40, 3, 0, 19, 22])) ;
+
+
+//Is the number divisible by the other number challenge;
+
+const isDivisible = (num1, num2) => {
+    if(num1 % num2 == 0){
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(isDivisible(4, 2));
+console.log(isDivisible(9, 3));
+console.log(isDivisible(15, 4));
