7348f7dfdb049f289bb6e32aa2d42b1387af965a
Isaac Endicott <endicottkai@gmail.com>
2023-05-21 23:57:21 -0600

    finished multi and sub functionality

--- level-two-assignments/simple-html-calculator/main.js
+++ level-two-assignments/simple-html-calculator/main.js
@@ -17,3 +17,42 @@ addForm.addEventListener("submit", function(event){
     li.textContent = numOne + " + " + numTwo + " = " + total
     results.appendChild(li)
 })
+
+//Subtraction form and functionality 
+const subForm = document.subForm;
+
+subForm.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    const numOne = subForm.subOne.valueAsNumber;
+    const numTwo = subForm.subTwo.valueAsNumber;
+    const results = document.getElementById("subResult")
+
+    subForm.subOne.value = ""
+    subForm.subTwo.value = ""
+
+    const total = numOne - numTwo;
+    const li = document.createElement("li")
+    li.textContent = numOne + " - " + numTwo + " = " + total
+    results.appendChild(li)
+})
+
+//Multiplication form and functionality 
+
+const multiForm = document.multiForm;
+
+multiForm.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    const numOne = multiForm.multiOne.valueAsNumber;
+    const numTwo = multiForm.multiTwo.valueAsNumber;
+    const results = document.getElementById("multiResult")
+
+    multiForm.multiOne.value = ""
+    multiForm.multiTwo.value = ""
+
+    const total = numOne * numTwo;
+    const li = document.createElement("li")
+    li.textContent = numOne + " * " + numTwo + " = " + total
+    results.appendChild(li)
+})
