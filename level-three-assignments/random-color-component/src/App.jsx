82071489a7fe51349c2d3d6ab2cfd210bb2d3044
Isaac Endicott <endicottkai@gmail.com>
2023-09-07 09:33:27 -0600

    finished ran color comp

---
+++ level-three-assignments/random-color-component/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  const [ranColor, setRanColor] = React.useState({})
+  const [count, setCount] = React.useState(1)
+  console.log(ranColor)
+  React.useEffect(function () {
+        fetch(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`)
+            .then(res => res.json())
+            .then(data => setRanColor(data.colors[0].hex))
+  }, [count])
+  return (
+    <>
+      <h1>Get your random color</h1>
+      <div style={{ backgroundColor:`#${ranColor}` }} className='box'></div>
+      <button className='button' onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Color</button>
+    </>
+  )
+}
+
+export default App
