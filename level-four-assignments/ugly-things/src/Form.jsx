ca77c06567d52337c8dcc3c89e0ff438c52c38c2
Isaac Endicott <endicottkai@gmail.com>
2023-11-06 11:52:37 -0700

    added a basic form with inputs

---
+++ level-four-assignments/ugly-things/src/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Form(){
+    return (
+        <form>
+            <input placeholder="Title"></input>
+            <input placeholder="Img URL"></input>
+            <input placeholder="Description"></input>
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default Form;
