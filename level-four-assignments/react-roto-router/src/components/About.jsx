6268c23c1d92cd038edb1ea3ef6dab2f2d3d47e0
Isaac Endicott <endicottkai@gmail.com>
2023-11-26 14:27:07 -0700

    finished roto router

---
+++ level-four-assignments/react-roto-router/src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function About(){
+    return (
+        <>
+            <h2>We are Plumbing Co.</h2>
+            <section>
+                <div class="about-content">
+                    <p><strong>Our Origins:</strong> Your Plumbing Company was founded in 1990 by the Smith family. What started as a small, local business has grown into a trusted name in plumbing services across the region.</p>
+
+                    <p><strong>Company Motto:</strong> "Reliable Plumbing, Trusted Service." This motto reflects our commitment to providing reliable and efficient plumbing solutions while earning the trust of our valued customers.</p>
+
+                    <p><strong>Company Motivation:</strong> Our motivation stems from the belief that everyone deserves quality plumbing services. We understand the inconvenience that plumbing issues can cause, and our team is dedicated to resolving problems promptly and effectively.</p>
+
+                    <p><strong>Family Run Business:</strong> Your Plumbing Company is proud to be a family-run business. The values of trust, integrity, and a strong work ethic have been passed down through generations, creating a company culture that feels like family.</p>
+
+                    <p><strong>Strong Moral Values:</strong> At the core of our business are strong moral values. We believe in honesty, transparency, and treating our customers with the utmost respect. Our goal is not just to fix plumbing issues but to build lasting relationships with our clients based on trust and reliability.</p>
+                </div>
+            </section>
+        </>
+    )
+}
---
+++ level-four-assignments/react-roto-router/src/components/Services.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Services(){
+    return (
+        <>
+            <h2>Services we offer</h2>
+            <section>
+                <div>
+                    <div><strong>1. Emergency Repairs</strong></div>
+                    <div>
+                        <p>We offer 24/7 emergency plumbing repairs for issues such as burst pipes, leaks, and other urgent problems. Our team is ready to respond promptly to your call and resolve the issue efficiently.</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div><strong>2. Drain Cleaning</strong></div>
+                    <div>
+                        <p>Our professional plumbers use advanced equipment to clear clogged drains and ensure smooth water flow. Whether it's a simple blockage or a more complex issue, we have the expertise to tackle it.</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div><strong>3. Fixture Installation</strong></div>
+                    <div>
+                        <p>We provide installation services for various fixtures, including faucets, sinks, toilets, and showers. Our team ensures proper installation to prevent future issues and maximize the lifespan of your fixtures.</p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
