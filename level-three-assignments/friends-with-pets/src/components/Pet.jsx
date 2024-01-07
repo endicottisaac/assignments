8f946e362d0b431e14be926fcf370013ed502650
Isaac Endicott <endicottkai@gmail.com>
2024-01-07 15:46:40 -0700

    a lot of changes

--- level-three-assignments/friends-with-pets/src/components/Pet.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Pet(props) {
-    return (
-        <div className="friend">
-            <h4>· Pet Name: {props.pets.name} ({props.pets.breed})</h4>
-        </div>
-    )
-}
--- level-three-assignments/set-up-with-vite/vite-project/src/NewHeader.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function NewHeader(){
-    return (
-        <h1>I love React</h1>
-    )
-}
